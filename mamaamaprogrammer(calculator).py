b67a28d03ff3a2a4b1d89a60db706029e8e1ff61
Mantool <91246152+Mantool@users.noreply.github.com>
2022-01-18 19:53:46 +0300

    Add files via upload

---
+++ mamaamaprogrammer(calculator).py
@@ -0,0 +1,15 @@
+num1 = float(input())
+num2 = float(input())
+operation = input()
+if operation == '+':
+    result = a + b
+    print(result)
+elif operation == '*':
+    result = a * b
+    print(result)
+elif operation == '/':
+    result = a / b
+    print(result)
+elif operation == '-':
+    result = a - b
+    print(result)
